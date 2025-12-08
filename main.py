b61746b8b132c031a2c92f255fbf0deda98b897f
Owen Hobbs <owenhobbs2006@gmail.com>
2025-12-07 19:32:01 -0600

    Add separate files

--- main.py
+++ main.py
@@ -1,161 +1,6 @@
 import cv2 as cv
-import numpy as np
-import math
-
-def line_x_at_y(x1, y1, x2, y2, y):
-    if y2 == y1: # if line is perfectly horizontal
-        return (x1 + x2) / 2.0 # use midpoint of x
-    # consider parameter t
-    # x(t) = x1 + t(x2 - x1)
-    # y(t) = y1 + t(y2 - y1)
-    # use y to solve for parameter t
-    t = (y - y1) / (y2 - y1)
-    # use parameter t to solve for x
-    return x1 + t * (x2 - x1)
-
-def compute_lateral_error(left, right, frame_width, ref_y, hfov_deg):
-    x1L, y1L, x2L, y2L = left
-    x1R, y1R, x2R, y2R = right
-
-    xL = line_x_at_y(x1L, y1L, x2L, y2L, ref_y)
-    xR = line_x_at_y(x1R, y1R, x2R, y2R, ref_y)
-
-    runway_width = xR - xL
-    if runway_width == 0:
-        return 0.0
-    centerline_x = (xL + xR) / 2.0 # average of xL and xR
-    image_center_x = frame_width / 2.0
-    dx_pixels = image_center_x - centerline_x
-
-    # lateral error in runway widths
-    # +1 = 1 runway-width right, -1 = left
-    lateral_error_rw = dx_pixels / runway_width
-
-    hfov_rad = math.radians(hfov_deg)
-    # compute focal length in pixels from hfov
-    fx = (frame_width / 2.0) / math.tan(hfov_rad / 2.0)
-
-    lat_err_rad = math.atan(dx_pixels / fx)
-    lat_err_deg = math.degrees(lat_err_rad)
-
-    return lateral_error_rw, lat_err_deg
-
-# def is_coincident(x1, y1, x2, y2, threshold=10.0):
-#     return abs(x2 - x1) < threshold and abs(y2 - y1) < threshold
-#
-# def is_corner(line1, line2, threshold=10.0):
-#     x1, y1, x2, y2 = line1
-#     x3, y3, x4, y4 = line2
-#     if is_coincident(x1, y1, x3, y3, threshold): return True
-#     if is_coincident(x1, y1, x4, y4, threshold): return True
-#     if is_coincident(x2, y2, x3, y3, threshold): return True
-#     if is_coincident(x2, y2, x4, y4, threshold): return True
-#     return False
-
-def is_perpendicular(line1, line2, angle_tolerance_deg=15.0):
-    x1, y1, x2, y2 = line1
-    x3, y3, x4, y4 = line2
-
-    dx1, dy1 = x2 - x1, y2 - y1
-    dx2, dy2 = x4 - x3, y4 - y3
-
-    mag1 = math.hypot(dx1, dy1)
-    mag2 = math.hypot(dx2, dy2)
-    if mag1 == 0 or mag2 == 0:
-        return False
-
-    # cos(theta) = (v1·v2)/(|v1||v2|)
-    dot_product = dx1 * dx2 + dy1 * dy2
-    cos_theta = dot_product / (mag1 * mag2)
-
-    # Clamp cos_theta [-1.0, 1.0] to use inverse cos
-    cos_theta = max(-1.0, min(1.0, cos_theta))
-    theta = math.degrees(math.acos(cos_theta))
-
-    return abs(theta - 90.0) <= angle_tolerance_deg
-
-def detect_runway_edges(frame):
-    # Preprocess
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    blur = cv.GaussianBlur(gray, (5, 5), 0) # 0 means OpenCV auto picks sigma
-
-    # Edge detection on whole image
-    # lower and upper thresholds for hysteresis procedure
-    # if greater than upper threshold than it is definitely an edge
-    # if lower than lower threshold it is not an edge and discarded
-    # if in between thresholds then it is weak edge and only kept if connected to strong edge
-    edges = cv.Canny(blur, 50, 150)
-    # debug
-    cv.imshow("Edges", edges)
-
-    # Hough line detection
-    lines = cv.HoughLinesP(
-        edges,
-        rho=1, # consider distances in steps of 1 pixel
-        theta=np.pi/180, # 1 degree steps
-        threshold=100, # num of votes
-        minLineLength=100, # pixels
-        maxLineGap=20, # gap between two collinear segments
-    )
-
-    if lines is None:
-        return None, frame
-
-    output_frame = frame.copy()
-    vertical_lines = []
-    horizontal_lines = []
-    for line in lines:
-        x1, y1, x2, y2 = line[0]
-        # find vertical lines using check: |dy|/|dx| > slope
-        # use 3 for strict vertical, lower to allow more slant
-        slope = 1.25
-        if abs(y2 - y1) > abs(x2 -x1) * slope:
-            # line is vertical
-            vertical_lines.append((x1, y1, x2, y2))
-            # draw it blue
-            cv.line(output_frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-        else:
-            # line is horizontal
-            horizontal_lines.append((x1, y1, x2, y2))
-            # draw it red
-            cv.line(output_frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
-    # sort horizontal_lines using average of y1 and y2
-    horizontal_lines = sorted(horizontal_lines, key=lambda L: (L[1] + L[3]) / 2.0, reverse=True)
-
-    # loop through horizontal lines bottom up until at least two connected edges are found
-    possible_lines = []
-    for horizontal_line in horizontal_lines:
-        connected_lines = [] # reset
-        for line in lines: # loop through lines instead?
-            line = tuple(line[0])
-            if line == horizontal_line:
-                continue
-            if is_perpendicular(horizontal_line, line):
-                # hx1, hy1, hx2, hy2 = horizontal_line
-                # cv.line(output_frame, (hx1, hy1), (hx2, hy2), (255, 255, 0), 2)
-                connected_lines.append(line)
-        if len(connected_lines) >= 2:
-            possible_lines = connected_lines
-            # Draw horizontal_line blue
-            hx1, hy1, hx2, hy2 = horizontal_line
-            cv.line(output_frame, (hx1, hy1), (hx2, hy2), (255, 255, 0), 2)
-            break
-
-    if len(possible_lines) < 2:
-        return None, frame
-
-    # Take leftmost and rightmost as runway edges
-    # sort using average of x1 and x2
-    possible_lines = sorted(possible_lines, key=lambda L: (L[0] + L[2]) / 2.0)
-    left = possible_lines[0]
-    right = possible_lines[-1]
-
-    # Draw runway edges green
-    cv.line(output_frame, (left[0], left[1]), (left[2], left[3]), (0, 255, 0), 2)
-    cv.line(output_frame, (right[0], right[1]), (right[2], right[3]), (0, 255, 0), 2)
-
-    return (left, right), output_frame
+import runway
+import edge_detection
 
 # initialize with external webcam
 cap = cv.VideoCapture(1)
@@ -165,6 +10,7 @@ if not cap.isOpened():
 
 lat_err_rw = 0
 lat_err_deg = 0
+vert_err = 0
 while True:
     # capture frame-by-frame
     ret, frame = cap.read()
@@ -174,22 +20,28 @@ while True:
         break
 
     # display_frame is copy of frame but with runway edges highlighted
-    runway_edges, display_frame = detect_runway_edges(frame)
+    runway_edges, display_frame = edge_detection.detect_runway_edges(frame)
     h, w, _ = display_frame.shape
 
     if runway_edges is not None:
-        left, right = runway_edges
+        left, right, base_line = runway_edges
 
-        # Calculate lateral deviation at ref_y
-        ref_y = int(h * 0.5) # use middle y-axis
-        lat_err_rw, lat_err_deg = compute_lateral_error(left, right, w, ref_y, hfov_deg=70)
+        # Calculate lateral deviation
+        left_x = (left[0] + left[2]) / 2.0 # average x1 and x2
+        right_x = (right[0] + right[2]) / 2.0  # average x1 and x2
+        lat_err_rw, lat_err_deg = runway.compute_lateral_error(left_x, right_x, w, hfov_deg=70)
 
-        # Display reference line
-        # cv.line(display_frame, (0, ref_y), (w, ref_y), (255, 0, 0), 1)
+        # Calculate vertical deviation
+        base_y = (base_line[1] + base_line[3]) / 2.0 # average y1 and y2
+        vert_err = runway.compute_vertical_error(base_y, h, ideal_base_y_frac=0.70)
 
     # Display lateral deviation
     cv.putText(display_frame, f"Lateral error: {lat_err_rw:+.2f} RW widths, {lat_err_deg:+.1f} deg",
                 (20, 40), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
+
+    # Display vertical deviation
+    cv.putText(display_frame, f"Vertical error: {vert_err:+.2f}",
+               (20, 70), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
 
     cv.imshow("Visual Runway Alignment Assist", display_frame)
     if cv.waitKey(1) == ord('q'):  # q to quit
---
+++ runway.py
@@ -0,0 +1,29 @@
+import math
+
+def compute_lateral_error(left_x, right_x, frame_width, hfov_deg):
+    runway_width = right_x - left_x
+    if runway_width == 0:
+        return 0.0
+    centerline_x = (left_x + right_x) / 2.0 # average of xL and xR
+    image_center_x = frame_width / 2.0
+    dx_pixels = image_center_x - centerline_x
+
+    # lateral error in runway widths
+    # +1 = 1 runway-width right, -1 = left
+    lateral_error_rw = dx_pixels / runway_width
+
+    hfov_rad = math.radians(hfov_deg)
+    # compute focal length in pixels from hfov
+    fx = (frame_width / 2.0) / math.tan(hfov_rad / 2.0)
+
+    lat_err_rad = math.atan(dx_pixels / fx)
+    lat_err_deg = math.degrees(lat_err_rad)
+
+    return lateral_error_rw, lat_err_deg
+
+def compute_vertical_error(base_y, frame_height, ideal_base_y_frac):
+    ideal_base_y = frame_height * ideal_base_y_frac
+
+    # TODO: improve
+    vert_err = (base_y - ideal_base_y)
+    return vert_err
