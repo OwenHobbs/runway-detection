ccd5e54ee89bddb45a5e91fa3f5a8c5595991e7e
rahmo004@umn.edu <165223781+HamzaRahmoune@users.noreply.github.com>
2025-12-07 21:12:34 -0600

    Implement yolo model

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+import os
+import sys
 import cv2 as cv
 import runway
 import edge_detection
+from ultralytics import YOLO
+
+# Load model
+if (not os.path.exists("runway_model.pt")):
+    print("ERROR: Model not found.")
+    sys.exit(0)
+model = YOLO("runway_model.pt", task='detect')
 
 # initialize with external webcam
-cap = cv.VideoCapture(1)
+cap = cv.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
+resW, resH = 1280, 720
+cap.set(3, resW) # width
+cap.set(4, resH) # height
 
 lat_err_rw = 0
 lat_err_deg = 0
@@ -19,31 +31,44 @@ while True:
         print("Cannot read camera")
         break
 
-    # display_frame is copy of frame but with runway edges highlighted
-    runway_edges, display_frame = edge_detection.detect_runway_edges(frame)
-    h, w, _ = display_frame.shape
+    # Resize frame to desired resolution
+    frame = cv.resize(frame,(resW,resH))
 
-    if runway_edges is not None:
-        left, right, base_line = runway_edges
+    # Run model!
+    results = model(frame, verbose=False)
+    detections = results[0].boxes
+
+    if len(detections) != 0:
+        # sort detections by confidence
+        detections = sorted(detections, key=lambda det: det.conf.item(), reverse=True)
+        # Use highest-confidence detection
+        runway_box = detections[0]
+        xyxy_tensor = runway_box.xyxy.cpu() # Detections in Tensor format in CPU memory
+        xyxy = xyxy_tensor.numpy().squeeze() # Convert tensors to Numpy array
+        xmin, ymin, xmax, ymax = xyxy.astype(int) # Extract individual coordinates and convert to int
+        h, w, _ = frame.shape
+
+        # Draw runway bounding box
+        cv.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 0, 255), 2)
 
         # Calculate lateral deviation
-        left_x = (left[0] + left[2]) / 2.0 # average x1 and x2
-        right_x = (right[0] + right[2]) / 2.0  # average x1 and x2
-        lat_err_rw, lat_err_deg = runway.compute_lateral_error(left_x, right_x, w, hfov_deg=70)
-
+        lat_err_rw, lat_err_deg = runway.compute_lateral_error(xmin, xmax, w, hfov_deg=70)
         # Calculate vertical deviation
-        base_y = (base_line[1] + base_line[3]) / 2.0 # average y1 and y2
-        vert_err = runway.compute_vertical_error(base_y, h, ideal_base_y_frac=0.70)
+        vert_err = runway.compute_vertical_error(ymax, h, ideal_base_y_frac=0.70)
+    else:
+        # No runway detected
+        cv.putText(frame, f"Runway not detected",
+                (20, 100), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
     # Display lateral deviation
-    cv.putText(display_frame, f"Lateral error: {lat_err_rw:+.2f} RW widths, {lat_err_deg:+.1f} deg",
+    cv.putText(frame, f"Lateral error: {lat_err_rw:+.2f} RW widths, {lat_err_deg:+.1f} deg",
                 (20, 40), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
 
     # Display vertical deviation
-    cv.putText(display_frame, f"Vertical error: {vert_err:+.2f}",
+    cv.putText(frame, f"Vertical error: {vert_err:+.2f}",
                (20, 70), cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
 
-    cv.imshow("Visual Runway Alignment Assist", display_frame)
+    cv.imshow("Visual Runway Alignment Assist", frame)
     if cv.waitKey(1) == ord('q'):  # q to quit
         break
 
